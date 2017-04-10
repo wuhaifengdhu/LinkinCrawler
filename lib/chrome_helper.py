f163f83952de897934d120930ee3066cd3d43873
Wu Haifeng <wuhaifengdhu@163.com>
2017-04-10 13:29:27 +0800

    fixed page load not complete problem

--- lib/chrome_helper.py
+++ lib/chrome_helper.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import re
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 
 
 class Chrome(object):
@@ -16,12 +21,8 @@ class Chrome(object):
         os.environ["webdriver.chrome.driver"] = driver_path
         return webdriver.Chrome(driver_path)
 
-    def _store_html(self, web_source, file_name='web.html'):
-        file_output = open(file_name, "w")
-        file_output.write(web_source)
-        file_output.close()
 
-    def pre_login(self):
+    def _pre_login(self):
         login_url = "https://www.linkedin.com/uas/login-cap"
         self.driver.get(login_url)
         self.driver.find_element_by_id('session_key-login').send_keys("wulinkedinmem@gmail.com")
@@ -29,11 +30,27 @@ class Chrome(object):
         self.driver.find_element_by_id("btn-primary").click()
         time.sleep(2)
 
+    def _generate_job_detail_url(self):
+        url_list = []
+        pass
+
+    @staticmethod
+    def _find_job_id(self, web_source):
+        job_id = list(set(re.findall('SearchJobJserp":{"jobPosting":"urn:li:fs_normalized_jobPosting:(\d*)"',
+                                     web_source)))
+        return job_id
+
     def open_url(self, url):
         self.driver.get(url)
-        time.sleep(3)
-        html_doc = self.driver.page_source.encode("utf-8")
-        self._store_html(html_doc)
+        delay = 3  # seconds
+        try:
+            WebDriverWait(self.driver, delay).until(
+                EC.presence_of_element_located((By.ID, 'clientPageInstance')))
+            print "Page is ready!"
+            html_doc = self.driver.page_source.encode("utf-8")
+            self._find_job_id(html_doc)
+        except TimeoutException:
+            print "Loading took too much time!"
 
     def base_login(self):
         self.driver.find_element_by_id('jobs-tab-icon').click()
@@ -46,4 +63,4 @@ class Chrome(object):
 if __name__ == "__main__":
     chrome = Chrome()
     chrome.pre_login()
-    chrome.open_url("https://www.linkedin.com/jobs/search/?keywords=Data%20Scientist&locationId=sg%3A0&start=0")
+    chrome.open_url("https://www.linkedin.com/jobs/search/?keywords=Data%20Scientist&locationId=sg%3A0&start=25")
